36d49ec1a0dbc72f9e4a0c199e12336eab2004be
Tong Zhou <tong.zhou@indexexchange.com>
2021-06-10 23:34:13 -0400

    cors allow

--- src/providers/request/Request.ts
+++ src/providers/request/Request.ts
@@ -5,7 +5,11 @@ export type IRequestFunc = (request: IRequest) => Promise<Response>;
 const request: IRequestFunc = (request: IRequest): Promise<Response> => {
     // we can swap out fetch with another library later on if needed
     const [url, options] = request;
-    return fetch(url, options);
+    const corsOptions: RequestInit = {
+        mode: 'cors',
+        ...options
+    };
+    return fetch(url, corsOptions);
 }
 
 export default request;
