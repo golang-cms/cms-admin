7b65a684ad0b93083e795768f57e8d0c5224f1ff
Hiu Kwan Cheung <hiukwancheung@Hius-Mac-mini.local>
2021-06-21 23:49:49 -0400

    create landing routes

---
+++ src/landing/Routes.tsx
@@ -0,0 +1,10 @@
+import { Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+  <Switch>
+    <Route path={LandingBasePath} exact />
+  </Switch>;
+};
+
+export const LandingBasePath = "";
+export default Routes;
