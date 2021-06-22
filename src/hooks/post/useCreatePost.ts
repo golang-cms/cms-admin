d7c98fec1fe24e71322e882eacd6a29b01209bf6
Tong Zhou <tong.zhou@indexexchange.com>
2021-06-21 22:32:52 -0400

    add config

--- src/hooks/post/useCreatePost.ts
+++ src/hooks/post/useCreatePost.ts
@@ -3,8 +3,6 @@ import { PostModel } from "../../admin/moudles/post/model/post";
 import { IRequest } from "../../providers/request/Request";
 import useApiResult from "../useApiResult";
 
-const CONTENT_API_BASE_URL = "https://cms-api-content-api-cms-zt1983811.cloud.okteto.net/v1";
-
 const useCreatePost = (post?: PostModel) => {
     //TODO may use it later for cache
     const request = useMemo(() => createPost(post), [post]);
@@ -13,7 +11,7 @@ const useCreatePost = (post?: PostModel) => {
 
 const createPost = (post?: PostModel): IRequest => {
     return [
-        `${CONTENT_API_BASE_URL}/posts`,
+        `${process.env.REACT_APP_CONTENT_API_BASE_URL}/posts`,
         {
             body: JSON.stringify(post),
             method: "POST",
--- src/hooks/post/useUpdatePost.ts
+++ src/hooks/post/useUpdatePost.ts
@@ -3,8 +3,6 @@ import { PostModel } from "../../admin/moudles/post/model/post";
 import { IRequest } from "../../providers/request/Request";
 import useApiResult from "../useApiResult";
 
-const CONTENT_API_BASE_URL = "https://cms-api-content-api-cms-zt1983811.cloud.okteto.net/v1";
-
 const useUpdatePost = (post?: PostModel) => {
     //TODO may use it later for cache
     const request = useMemo(() => updatePost(post), [post]);
@@ -13,7 +11,7 @@ const useUpdatePost = (post?: PostModel) => {
 
 const updatePost = (post?: PostModel): IRequest => {
     return [
-        `${CONTENT_API_BASE_URL}/posts/${post?.id}`,
+        `${process.env.REACT_APP_CONTENT_API_BASE_URL}/posts/${post?.id}`,
         {
             body: JSON.stringify(post),
             method: "PUT",
